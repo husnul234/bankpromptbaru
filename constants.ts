ff07b361b8b206b440f461ce124b095025157712
husnul234 <jagofeedmediatama@gmail.com>
2025-11-30 20:29:35 +0700

    Update GOOGLE_SCRIPT_URL to new deployment link

--- constants.ts
+++ constants.ts
@@ -1,6 +1,6 @@
 // PENTING: Paste Web App URL dari Google Apps Script di sini
 // Cara dapat URL: Extensions > Apps Script > Deploy > New Deployment > Web App > Who has access: Anyone > Deploy
-export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx1XKc0us1Dlzlvzd3DZ-Vz8xO0PXJb-a-Xrn9KOw56M3-AXGfohISINw5qOWCKmGJRhA/exec"; 
+export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyF6ltgx9K0pzyrFAYMwsNROjpbbhXUH0CVFHf3nAaMkRR3bmWc2ectbBNg4Ad6rMxg5w/exec"; 
 
 export const ADMIN_PASSWORD = "G@dingraya25";
 export const LOGO_URL = "https://files.catbox.moe/zesag4.jpg";
